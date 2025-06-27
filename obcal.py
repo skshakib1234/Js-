298c004b9a4b08e6dc4917159bdc470347d8db80
skshakib1234 <111051965+skshakib1234@users.noreply.github.com>
2025-06-27 10:20:29 +0600

    Add files via upload

---
+++ obcal.py
@@ -0,0 +1,39 @@
+# Valid POI Calculator
+def POI2(value):
+  if ':' in value:
+    tm = value.split(":")
+    sum1 = int(tm[0]) + int(tm[1])
+    if (sum1 > 9):
+      result = int(sum1[0]) + int(sum1[1])
+      if [3,6,9] in result:
+        print("Your POI is valid")
+      else:
+        print("Your POI is invalid") 
+    elif [3,6,9] in sum1:
+      print("Your POI is valid")
+    else:
+      print("Your POI is invalid") 
+  elif ':' not in value:
+    val2 = int(value)
+    if (val2 > 9):
+      sum2 = val2[0] + val2[1]
+      if [3,6,9] in sum2:
+        print("Your POI is valid")
+      else:
+        print("Your POI is invalid")
+    elif [3,6,9] in sum2:
+      print("Your POI is valid")
+    else:
+      print("Your POI is invalid") 
+def POI():
+  print(" POI timing in date use 1\n POI timing in time use 2")
+  ptm = int(input(" Select :"))
+  if (ptm==1):
+    time = input("Enter date : ")
+    POI2(time)
+  elif (ptm==2):
+    time = input("Enter time(12:00) : ")
+    POI2(time)
+  else:
+    sys.exit()
+POI()
